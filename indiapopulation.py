d9dfa46eb96a74deb625337de49867f9b1d325f8
Vinay Kumar <57426152+Vinaykumarkummarikuntla@users.noreply.github.com>
2022-05-21 23:08:57 +0530

    Add files via upload

---
+++ indiapopulation.py
@@ -0,0 +1,46 @@
+import csv
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+def addlabels(length, population):
+    """
+    for Adding labels on top of bar plot
+    """
+    for value in range(length):
+        plt.text(value, population[value], population[value], ha="center")
+
+
+def indian_population():
+    """
+    Bar Plot of 'population of India' vs. years.
+    """
+    with open(r'D:\UN POPULATION\population-estimates (1).csv',encoding='utf-8') as file:
+        csv_list = list(csv.DictReader(file, delimiter=','))
+        file.close()
+
+    india = defaultdict(int)
+
+    for row in csv_list:
+        if row['Region'] == "India":
+            india[row['Year']] = int(float(row['Population']))
+
+    # taking the recent 10 years of data
+    years = 10
+
+    # passing the parameters to the bar function,
+    # this is the main function which creates the bar plot
+    plt.bar(list(india.keys())[-years:], list(india.values())[-years:])
+    addlabels(years, list(india.values())[-years:])
+    plt.title('Indian population over years')
+    plt.xlabel('Years')
+    plt.ylabel('Population')
+    plt.xticks(rotation=25)
+
+    # Displaying the bar plot
+    plt.show()
+
+
+if __name__ == '__main__':
+    indian_population()
+    
---
+++ saarc.py
@@ -0,0 +1,59 @@
+"""
+Total SAARC population vs year
+"""
+# importing the required libraries
+import csv
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+def addlabels(length, population):
+    """
+    for Adding labels on top of bar plot
+    """
+    for value in range(length):
+        plt.text(value, population[value], population[value], ha="center")
+
+
+def total_saarc_population():
+    """
+    Total SAARC population vs year
+    """
+    with open(r'D:\UN POPULATION\population-estimates (1).csv',encoding='utf-8') as file:
+        population_data = list(csv.DictReader(file, delimiter=','))
+        file.close()
+
+    with open(r'D:\UN POPULATION\saarc.csv', encoding='utf-8') as file:
+        saarc_list = [row['saarc countries'] for row in csv.DictReader(file)]
+        file.close()
+
+    years = [
+             "2005", "2006", "2007", "2008",
+             "2009", "2010", "2011", "2012",
+             "2013", "2014", "2015"
+            ]
+    saarc = defaultdict(int)
+
+    for row in population_data:
+        if row['Region'] in saarc_list:
+            if row['Year'] in years:
+                saarc[row['Year']] += int(float(row['Population']))
+
+    year = list(saarc.keys())
+    population = list(saarc.values())
+
+    # Passing the parameters to the bar function,
+    # this is the main function which creates the bar plot
+    plt.bar(year, population)
+    addlabels(len(year), population)
+    plt.title('Total SAARC population vs year')
+    plt.xlabel('Years')
+    plt.ylabel('Population')
+    plt.xticks(rotation=25)
+
+    # Displaying the bar plot
+    plt.show()
+
+
+if __name__ == '__main__':
+    total_saarc_population()
